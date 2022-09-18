57be23ca5d7329cdc1859a2eb99fd5030462aae2
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-09-18 15:16:33 -0300

    changeUsernameAndImage

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,19 @@
+import React from 'react';
 export default function(){
+    const [profilePic, setprofilePic] = React.useState("assets/img/catanacomics.svg");
+    const [user, setUser] = React.useState("Catana");
+    const [displayName, setDisplayName] = React.useState("catanacomis");
+
     return(
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img onClick = {()=>setprofilePic(prompt("Insira o url da imagem nova: "))}src={profilePic} />
             <div class="texto">
-                <strong>catanacomics</strong>
+                <strong>{displayName}
+                <ion-icon onClick={() => setDisplayName(prompt("Digite o seu novo displayname: "))}  name="pencil"></ion-icon>
+                </strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {user}
+                    <ion-icon onClick={() => setUser(prompt("Digite o seu novo username: "))} name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
