478798487e2c367e5169877eedca04d54c3b5173
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-09-18 14:14:35 -0300

    comecoComponentes

---
+++ src/Usuario.js
@@ -0,0 +1,16 @@
+export default function(){
+    return(
+        <div class="usuario">
+            <img src="assets/img/catanacomics.svg" />
+            <div class="texto">
+                <strong>catanacomics</strong>
+                <span>
+                    Catana
+                    <ion-icon name="pencil"></ion-icon>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+
