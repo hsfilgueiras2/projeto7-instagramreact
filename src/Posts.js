777ff43d0fb1386b7da3a04fb878512dc9013d96
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-09-19 17:40:24 -0300

    entrega

--- src/Posts.js
+++ src/Posts.js
@@ -1,37 +1,37 @@
 
 import React from "react";
 export default function Posts(){
-    const posts = [{user:"meowed",img:"assets/img/gato-telefone.svg", curtidas:101.523, ultimaCurtida:"respondeai"},
-    {user:"barked",img:"assets/img/dog.svg", curtidas:99.159, ultimaCurtida:"adorable_animals" }]
+    const posts = [{user:"meowed",img:"assets/img/gato-telefone.svg", curtidas:101.523, ultimaCurtida:"respondeai",state:"heart-outline"},
+    {user:"barked",img:"assets/img/dog.svg", curtidas:99.159, ultimaCurtida:"adorable_animals",state:"heart-outline" }]
     const handleClick = (e) => {
         const attr = e.target.getAttribute("name");
-        // Check for attributes
         if (!attr) {
           return;
         }
-        // Switch status
         const changeAttr = attr == "bookmark-outline" ? "bookmark" : "bookmark-outline";
-        // Set result back
         e.target.setAttribute("name", changeAttr);
       };
 
       const handleClick2 = (e) => {
-        const attr = e.target.getAttribute("name");
-        // Check for attributes
-        if (!attr) {
-          return;
-        }
-        // Switch status
 
-        const changeAttr = attr == "heart-outline" ? "heart" : "heart-outline";
+        const attr = e.target.getAttribute("data-arrayPos");
+        console.log("attr:"+attr);
+        const targetDiv = document.querySelector(`.like${attr}`);
+        const likeIcon = targetDiv.querySelector("ion-icon");
+        const check = posts[attr].state;
+        const texto = targetDiv.querySelector(".texto")
+        const changeAttr = check == "heart-outline" ? "heart" : "heart-outline";
+        console.log(targetDiv.classList)
         if (changeAttr == "heart"){
-            e.target.setAttribute("class","vermelho")
+            likeIcon.setAttribute("class","vermelho")
+            posts[attr].curtidas ++;
         }
         else{
-            e.target.setAttribute("class", "")
+            likeIcon.setAttribute("class", "")
+            posts[attr].curtidas --;
         }
-        // Set result back
-        e.target.setAttribute("name", changeAttr);
+        posts[attr].state=changeAttr;
+        likeIcon.setAttribute("name",posts[attr].state)
       };
     return(
         <div class="posts">
@@ -47,15 +47,15 @@ export default function Posts(){
                     <ion-icon name="ellipsis-horizontal"></ion-icon>
                     </div>
                 </div>
-                <div data-arrayPos={posts.indexOf(item)}>
-                <div class="conteudo">
-                    <img src={item.img} />
+                <div class={`like`+posts.indexOf(item)}>
+                <div data-arrayPos={posts.indexOf(item)} onClick={handleClick2} class="conteudo">
+                    <img data-arrayPos={posts.indexOf(item)} src={item.img} />
                 </div>
 
                 <div class="fundo">
                     <div class="acoes">
                     <div>
-                        <ion-icon onClick={handleClick2} name={"heart-outline"}></ion-icon>
+                        <ion-icon data-findLike={posts.indexOf(item)} data-arrayPos={posts.indexOf(item)} onClick={handleClick2} name={item.state}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
@@ -70,7 +70,7 @@ export default function Posts(){
                         Curtido por <strong>{item.ultimaCurtida}</strong> e <strong>outras {item.curtidas} pessoas</strong>
                     </div>
                     </div>
-                </div>
+                    </div>
                 </div>
             </div>
                 )
